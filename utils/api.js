5bdc90eeeee6e561c117becb5a8a2aa379786f7d
Kenya Sullivan <kenya@stackpoets.com>
2017-11-21 16:48:32 -0500

    Add API and Colors utils

---
+++ utils/api.js
@@ -0,0 +1,35 @@
+import { AsyncStorage } from "react-native";
+
+const DECKS_STORAGE_KEY = "Flashcards:decks";
+
+export function getDecks() {
+  return AsyncStorage.getItem(DECKS_STORAGE_KEY).then(results => {
+    return JSON.parse(results);
+  });
+}
+
+export function getDeck(id) {
+  return decks[id];
+}
+
+export function saveDeckTitle(deck) {
+  return AsyncStorage.mergeItem(
+    DECKS_STORAGE_KEY,
+    JSON.stringify({
+      [deck.title]: deck
+    })
+  );
+}
+
+export function addCardToDeck(title, card) {
+  getDecks().then(decks => {
+    const deck = decks[title];
+    deck.cards.push(card);
+    return saveDeckTitle({ [title]: deck });
+  });
+}
+
+// getDecks: return all of the decks along with their titles, questions, and answers.
+// getDeck: take in a single id argument and return the deck associated with that id.
+// saveDeckTitle: take in a single title argument and add it to the decks.
+// addCardToDeck: take in two arguments, title and card, and will add the card to the list of questions for the deck with the associated title.
--- utils/colors.js
+++ utils/colors.js
@@ -1,2 +1,7 @@
 export const blue = "#0188D0";
 export const white = "#fff";
+export const green = "#39BC57";
+export const offwhite = "#F8F8F8";
+export const darkgray = "#4A4A4A";
+export const lightgray = "#DDD";
+export const lightblue = "#2699FB";
